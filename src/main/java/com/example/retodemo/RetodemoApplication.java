c866f7df0363e16f2ba15423e8cb2778af4d6ea6
Elizar Pongracz <elizar.pongracz@carvolution.ch>
2022-12-26 15:02:42 +0100

    Initial commit

---
+++ src/main/java/com/example/retodemo/RetodemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.retodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetodemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RetodemoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/retodemo/RetodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.retodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetodemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
